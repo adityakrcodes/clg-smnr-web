d1e7b9ace47fa3216e0517e59705cc97c219af85
Shadab <mitnic8@gmail.com>
2024-01-10 22:34:52 +0530

    Added Media Query

---
+++ index.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var arrowUp = document.getElementById("arrow-up");
+
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > window.innerHeight / 4) {
+            arrowUp.style.display = "block";
+        } else {
+            arrowUp.style.display = "none";
+        }
+    });
+
+    arrowUp.addEventListener("click", function () {
+        window.scrollTo({
+            top: 750,
+            behavior: "smooth",
+        });
+    });
+});
